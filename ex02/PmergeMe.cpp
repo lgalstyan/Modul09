08cd45613dbbb283606d60756badfcbf8c546003
lgalstyan <larisagalstya@gmail.com>
2023-07-30 20:56:18 +0400

    template

---
+++ ex02/PmergeMe.cpp
@@ -0,0 +1,11 @@
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe(){}
+
+PmergeMe::PmergeMe(const PmergeMe& ){}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& ){}
+
+PmergeMe::~PmergeMe(){}
+
+// PmergeMe::
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -1,6 +1,15 @@
 #ifndef PMERGEAME
 #define PMERGEAME
 
-
+class PmergeMe
+{
+    public:
+        PmergeMe();
+        PmergeMe(const PmergeMe& );
+        PmergeMe& operator=(const PmergeMe& );
+        ~PmergeMe();
+    private:
+        
+};
 
 #endif
