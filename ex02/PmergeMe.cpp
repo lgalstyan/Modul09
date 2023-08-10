4a7d9f7eecd157f86cd0e78a992eac02ba8ed88f
Larisa Galstyan <lgalstya@c2r17s1.42yerevan.am>
2023-08-10 15:02:37 +0400

    sorting done need to check

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -4,138 +4,162 @@ PmergeMe::PmergeMe(){}
 
 PmergeMe::PmergeMe(const PmergeMe& other)
 {
-    *this = other;
+	*this = other;
 }
 
 PmergeMe& PmergeMe::operator=(const PmergeMe& rhs)
 {
-    if (this != &rhs)
-    {
-        _vect = rhs._vect;
-        _list = rhs._list;
-        _t_vector_start = rhs._t_vector_start;
-        _t_vector_finish = rhs._t_vector_finish;
-        _t_list_start = rhs._t_list_start;
-        _t_list_finish = rhs._t_list_finish;
-    }
-    return (*this);
+	if (this != &rhs)
+	{
+		_vect = rhs._vect;
+		_deque = rhs._deque;
+		_t_vector_start = rhs._t_vector_start;
+		_t_vector_finish = rhs._t_vector_finish;
+		_t_deque_start = rhs._t_deque_start;
+		_t_deque_finish = rhs._t_deque_finish;
+	}
+	return (*this);
 }
 
 PmergeMe::~PmergeMe(){}
 
 void PmergeMe::sorting(int argc, char **argv)
 {
-    std::cout << "Before: " ;
-    print_unsorted(argc, argv);
-    std::cout << std::endl;
+	std::cout << "Before: " ;
+	print_unsorted(argc, argv);
+	std::cout << std::endl;
 
-    // for vector
-    _t_vector_start = get_current_time(&_time);
-    fill_vector(argc, argv);
-    //TODO write sort using vector
-    _t_vector_finish = get_current_time(&_time);
-
-
-    //for list
-    _t_list_start = get_current_time(&_time);
-    fill_list(argc, argv);
-    // print_list();
-    // //TODO write sort using list
-    _t_list_finish = get_current_time(&_time);
-
-    std::cout << "After: " ;
-    print_sorted();
-    std::cout << std::endl;
+	// for vector
+	_t_vector_start = get_current_time(&_time);
+	fill_vector(argc, argv);
+	sort_insertion(_vect);
+	//TODO write sort using vector
+	_t_vector_finish = get_current_time(&_time);
 
 
-    std::cout << "Time to process a range of " << argc << " elements with std::vector : " << (_t_vector_finish - _t_vector_start) / 1000 << " us\n";
-    std::cout << "Time to process a range of " << argc << " elements with std::list : " << (_t_list_finish - _t_list_start) / 1000 << " us\n";
+	// //for deque
+	// _t_deque_start = get_current_time(&_time);
+	// fill_deque(argc, argv);
+	// sort_insertion(_deque);
+	// // print_deque();
+	// // //TODO write sort using deque
+	// _t_deque_finish = get_current_time(&_time);
+
+	std::cout << "After: " ;
+	print_sorted();
+	std::cout << std::endl;
+
+
+	std::cout << "Time to process a range of " << argc << " elements with std::vector : " << (_t_vector_finish - _t_vector_start) / 1000 << " us\n";
+	std::cout << "Time to process a range of " << argc << " elements with std::deque : " << (_t_deque_finish - _t_deque_start) / 1000 << " us\n";
 }
 bool PmergeMe::areAllDigits(const std::string& str)
 {
-    std::string::const_iterator it = str.begin();
-    if (*it == '+')
-        ++it;
-    for (; it != str.end(); ++it)
-    {
-        if (!isdigit(*it)) {
-            return false;
-        }
-    }
-    return true;
+	std::string::const_iterator it = str.begin();
+	if (*it == '+')
+		++it;
+	for (; it != str.end(); ++it)
+	{
+		if (!isdigit(*it)) {
+			return false;
+		}
+	}
+	return true;
 }
 
 int PmergeMe::check_num(char *num)
 {
-    int res = -1;
-    std::string str(num);
-    std::string str1(num);
-    std::istringstream stream(str);
-    std::istringstream stream1(str);
-    
-    std::getline(stream, str, ' ');
-    std::istringstream convert(str);
-    convert >> res;
-    if (stream.fail() || (res == 0 && str[0] != '0') || !areAllDigits(str))
-        return -1;
-    if (stream.fail())
-    {
-        return -1;
-    }
-    return res;
+	int res = -1;
+	std::string str(num);
+	std::string str1(num);
+	std::istringstream stream(str);
+	std::istringstream stream1(str);
+
+	std::getline(stream, str, ' ');
+	std::istringstream convert(str);
+	convert >> res;
+	if (stream.fail() || (res == 0 && str[0] != '0') || !areAllDigits(str))
+		return -1;
+	if (stream.fail())
+	{
+		return -1;
+	}
+	return res;
 }
 
-void PmergeMe::fill_list(int argc, char **argv)
+void PmergeMe::fill_deque(int argc, char **argv)
 {
-    for (int i = 1; i < argc; ++i)
-    {
-        int tmp = check_num(argv[i]);
-        if (tmp < 0)
-        {
-            std::cerr << RED << "❌Error : invalid input!\n" << RESET;
-            exit(1);
-        }   
-        else 
-        {
-            _list.push_back(tmp);
-        }
-    }
+	for (int i = 1; i < argc; ++i)
+	{
+		int tmp = check_num(argv[i]);
+		if (tmp < 0)
+		{
+			std::cerr << RED << "❌Error : invalid input!\n" << RESET;
+			exit(1);
+		}   
+		else 
+		{
+			_deque.push_back(tmp);
+		}
+	}
 }
 
 void PmergeMe::fill_vector(int argc, char **argv)
 {
-    for (int i = 1; i < argc; ++i)
-    {
-        int tmp = check_num(argv[i]);
-        if (tmp < 0)
-        {
-            std::cerr << RED << "❌Error : invalid input!\n" << RESET;
-            exit(1);
-        }   
-        else 
-        {
-            _vect.push_back(tmp);
-        }
-    }
+	for (int i = 1; i < argc; ++i)
+	{
+		int tmp = check_num(argv[i]);
+		if (tmp < 0)
+		{
+			std::cerr << RED << "❌Error : invalid input!\n" << RESET;
+			exit(1);
+		}   
+		else 
+		{
+			_vect.push_back(tmp);
+		}
+	}
 }
 
 void PmergeMe::print_unsorted(int argc, char **argv)
 {
-    for (int i = 1; i < argc; ++i)
+    if (argc > LIMIT)
     {
-        std::cout << argv[i] << " ";
+	    for (int i = 1; i < LIMIT; ++i)
+	    {
+	    	std::cout << argv[i] << " ";
+	    }
+        std::cout << "[...]";
     }
+    else
+    {
+	    for (int i = 1; i < argc; ++i)
+	    {
+	    	std::cout << argv[i] << " ";
+	    }
+    }
+
 }
 
 void PmergeMe::print_sorted()
 {
-    for (std::vector<int>::const_iterator it = _vect.begin(); it != _vect.end(); ++it) {
-        std::cout << *it << " ";
+    if (_vect.size() > LIMIT)
+    {
+        int ind;
+        std::vector<int>::const_iterator it;
+	    for (ind = 0,  it = _vect.begin(); ind != LIMIT; ++it, ++ind)
+        {
+	    	std::cout << *it << " ";
+	    }
+        std::cout << "[...]";
     }
+	for (std::vector<int>::const_iterator it = _vect.begin(); it != _vect.end(); ++it) {
+		std::cout << *it << " ";
+	}
 }
 
-double	get_current_time(struct timeval *time)
+double	PmergeMe::get_current_time(struct timeval *time)
 {
 	gettimeofday(time, NULL);
-    return ((time->tv_sec * 1000000) + (time->tv_usec));
+	return ((time->tv_sec * 1000000) + (time->tv_usec));
 }
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -3,114 +3,132 @@
 
 #define RESET	"\033[0m"
 #define BLUE	"\033[1;34m"
-#define GREEN   "\x1B[32m"
+#define GREEN "\x1B[32m"
 #define RED		"\033[31m"
+#define LIMIT 5
 
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
-#include <list>
+#include <deque>
 #include <sys/time.h>
 #include <iomanip>
 
 class PmergeMe
 {
-    public:
-        PmergeMe();
-        PmergeMe(const PmergeMe& );
-        PmergeMe& operator=(const PmergeMe& );
-        ~PmergeMe();
-        void sorting(int , char** );
+	public:
+		PmergeMe();
+		PmergeMe(const PmergeMe& );
+		PmergeMe& operator=(const PmergeMe& );
+		~PmergeMe();
+		void sorting(int , char** );
 
-    private:
-        void fill_list(int , char **);
-        void fill_vector(int , char **);
-        void print_sorted();
-        void print_unsorted(int argc, char **argv);
-        int check_num(char *);
-        bool areAllDigits(const std::string& );
-        double	get_current_time(struct timeval* );
+	private:
+		void fill_deque(int , char **);
+		void fill_vector(int , char **);
+		void print_sorted();
+		void print_unsorted(int argc, char **argv);
+		int check_num(char *);
+		bool areAllDigits(const std::string& );
+		double	get_current_time(struct timeval* );
 
-    private:
-        std::vector<int> _vect;
-        std::list<int> _list;
-        struct timeval _time;
-        double _t_vector_start;
-        double _t_vector_finish;
-        double _t_list_start;
-        double _t_list_finish;
+	private:
+		std::vector<int> _vect;
+		std::deque<int> _deque;
+		struct timeval _time;
+		double _t_vector_start;
+		double _t_vector_finish;
+		double _t_deque_start;
+		double _t_deque_finish;
 };
 
 template<typename Data>
-void sort_insertion(Data vec_list)
+void print_vector(Data vec_deque)
 {
-    for (Data::iterator step = vec_list.begin(); step != vec_list.end(); ++step) 
-    {
-        Data::iterator key = step;
-        Data::iterator j = step - 1;
-
-        while (*key < *j && j >= 0)
-        {
-            j + 1 = j;
-            --j;
-        }
-        j + 1 = key;
-  }
+	for (typename Data::iterator step = vec_deque.begin(); step != vec_deque.end(); ++step) 
+	{
+		std::cout << *step << ' ';
+	}
+	std::cout << std::endl;
 }
 
 template<typename Data>
-void merge(Data arr, int p, int q, int r) {
-  int n1 = q - p + 1;
-  int n2 = r - q;
+void sort_insertion(Data &vec_deque)
+{
+  typename Data::iterator it1, it2;
 
-  Data L[n1];
-  Data M[n2];
-
-  for (int i = 0; i < n1; i++)
-    L[i] = arr[p + i];
-  for (int j = 0; j < n2; j++)
-    M[j] = arr[q + 1 + j];
-
-  int i, j, k;
-  i = 0;
-  j = 0;
-  k = p;
-
-  while (i < n1 && j < n2) {
-    if (L[i] <= M[j]) {
-      arr[k] = L[i];
-      i++;
-    } else {
-      arr[k] = M[j];
-      j++;
-    }
-    k++;
-  }
-
-  while (i < n1) {
-    arr[k] = L[i];
-    i++;
-    k++;
-  }
-
-  while (j < n2) {
-    arr[k] = M[j];
-    j++;
-    k++;
-  }
+	for (it1 = vec_deque.begin() + 1; it1 <= vec_deque.end(); ++it1)
+  {
+			int key = *it1;
+			it2 = it1;
+			while (it2 > vec_deque.begin() && key < *(it2 - 1))
+      {
+				*it2 = *(it2 - 1);
+				--it2;
+			}
+			*it2 = key;
+	}
 }
 
 template<typename Data>
-void mergeSort(Data arr, int l, int r) {
-  if (l < r) {
-    int m = l + (r - l) / 2;
+void merge(Data &arr, int left, int midl, int right)
+{
+	int n1 = midl - left + 1;
+	int n2 = right - midl;
 
-    mergeSort(arr, l, m);
-    mergeSort(arr, m + 1, r);
+	Data L[n1];
+	Data M[n2];
 
-    merge(arr, l, m, r);
-  }
+	for (int i = 0; i < n1; i++)
+		L[i] = arr[left + i];
+
+	for (int j = 0; j < n2; j++)
+		M[j] = arr[midl + 1 + j];
+
+	int i, j, k;
+	i = 0;
+	j = 0;
+	k = left;
+
+	while (i < n1 && j < n2)
+  {
+		if (L[i] < M[j]) {
+			arr[k] = L[i];
+			i++;
+		} else {
+			arr[k] = M[j];
+			j++;
+		}
+		k++;
+	}
+  
+	while (i < n1)
+  {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2)
+  {
+		arr[k] = M[j];
+		j++;
+		k++;
+	}
+}
+
+template<typename Data>
+void mergeSort(Data &arr, int l, int r) {
+	if (r - l <= LIMIT) {
+		insertionSort(arr.begin() + l, arr.begin() + r);
+	} else {
+		int m = l + (r - l) / 2;
+
+		mergeSort(arr, l, m);
+		mergeSort(arr, m + 1, r);
+		merge(arr, l, m, r);
+	}
 }
 
 #endif
